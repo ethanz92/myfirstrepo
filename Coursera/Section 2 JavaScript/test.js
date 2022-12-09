d593ca9f70af21409d800b639344e73cebd8cfbc
ethanz92 <109267902+ethanz92@users.noreply.github.com>
2022-12-09 22:28:34 +1100

    9DEC22

---
+++ Coursera/Section 2 JavaScript/test.js
@@ -0,0 +1,38 @@
+// function addTwoNums(a, b) {
+//   try {
+//     if (typeof a != "number") {
+//       throw new ReferenceError("the first argument is not a number");
+//     } else if (typeof b != "number") {
+//       throw new ReferenceError("the second argument is not a number");
+//     } else {
+//       console.log(a + b);
+//     }
+//   } catch (err) {
+//     console.log("Error!", err);
+//   }
+// }
+// addTwoNums(5, "5");
+// console.log("It still works");
+
+// function letterFinder(word, match) {
+//   let condition1 = typeof word == "string" && word.length >= 2;
+//   let condition2 = typeof match == "string" && match.length == 1;
+//   if (condition1 && condition2) {
+//     for (let i = 0; i < word.length; i++) {
+//       if (word[i] == match) {
+//         console.log("Found the", match, "at", i);
+//       } else {
+//         console.log("---No match found at", i);
+//       }
+//     }
+//   } else {
+//     console.log("Please pass correct arguments to the function.");
+//   }
+// }
+
+// letterFinder(2, 22);
+// letterFinder("cat", "c");
+
+const a = 6;
+a = 7;
+console.log(a);
